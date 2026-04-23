cff56b668b6e96bacc2be7f1f87c6e74ee51fc44
whitezaddy <austinihueze@gmail.com>
2026-04-23 21:38:09 +0100

    docs: finalize PR description and visual evidence for Ticket #19

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "@stellar/design-system/build/styles.min.css";
 import { WalletProvider } from "./providers/WalletProvider.tsx";
 import { NotificationProvider } from "./providers/NotificationProvider.tsx";
 import { ContractSyncProvider } from "./providers/ContractSyncProvider.tsx";
+import { TransactionProvider } from "./components/TransactionProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -21,15 +22,17 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <NotificationProvider>
-      <QueryClientProvider client={queryClient}>
-        <ContractSyncProvider>
-          <WalletProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </WalletProvider>
-        </ContractSyncProvider>
-      </QueryClientProvider>
-    </NotificationProvider>
+  <QueryClientProvider client={queryClient}>
+    <TransactionProvider>
+      <ContractSyncProvider>
+        <WalletProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </WalletProvider>
+      </ContractSyncProvider>
+    </TransactionProvider>
+  </QueryClientProvider>
+</NotificationProvider>
   </StrictMode>,
 );
